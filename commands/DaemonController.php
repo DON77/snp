d1c53ad514de2131fb2ac7a5e082443ad667333c
Gevorg Mansuryan <gevorgmansuryan@gmail.com>
2016-04-20 20:33:58 +0400

    daemon command

--- commands/DaemonController.php
+++ commands/DaemonController.php
@@ -12,7 +12,7 @@ class DaemonController extends Controller
     public function actionStart()
     {
         set_time_limit(0);
-        $daemon = new \app\components\SocketDaemon(Yii::$app->params['socketHost'], Yii::$app->params['socketPort']);
+        $daemon = new \app\components\SocketDaemon(\Yii::$app->params['socketHost'], \Yii::$app->params['socketPort']);
         $daemon->start();
     }
 }
